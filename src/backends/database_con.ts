b2b9a7ef84ed207cb31a6369cd4f0fd980627068
yamaan khan <84683423+yamaankhan20@users.noreply.github.com>
2024-11-07 21:37:10 +0500

    Update database_con.ts

--- src/backends/database_con.ts
+++ src/backends/database_con.ts
@@ -1,9 +1,16 @@
 import { Sequelize } from 'sequelize';
 
 // Database connection setup
-const sequelize = new Sequelize('sound-well', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('thesoundwell-vibro-therapy-server', 'avfxmkkera', 'EHyN$sIJ6hKAW7Y3', {
+    host: 'thesoundwell-vibro-therapy-server.mysql.database.azure.com',
     dialect: 'mysql',
+    port: 3306, // Default MySQL port, use if required
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // You might need this for secure connections
+        }
+    }
 });
 
 // Test the database connection
